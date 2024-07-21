d1b3f0a98eb8dcb267084daf9d293735510b102a
mitsui <171986182+mmIPA@users.noreply.github.com>
2024-07-21 11:13:51 +0000

    [Add] 削除機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,24 @@
 class BooksController < ApplicationController
+
+
+def new
+  @book = Book.new  
+end
+
+def create
+  book = Book.new(list_params)
+  book.save
+  redirect_to '/'
+end
+
+def index
+  @books = Book.all
+  @book = Book.new
+end
+
+  private
+  def list_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
