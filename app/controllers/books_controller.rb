4539b9b1c7de079573e3927e6d309ca77a38f050
mitsui <171986182+mmIPA@users.noreply.github.com>
2024-07-24 10:59:13 +0000

    update時にフラッシュメッセージを設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ def new
 end
 
 def create
-  @book = Book.new(list_params)
+  @book = Book.new(book_params)
   if @book.save
     flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book)
@@ -29,6 +29,17 @@ def edit
   @book = Book.find(params[:id])
 end
 
+def update
+  @book = Book.find(params[:id])
+  if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book)
+  else
+    flash[:alert] = @book.errors.full_messages
+    render :edit
+  end
+end
+
 def destroy
   book = Book.find(params[:id])
   book.destroy
@@ -36,7 +47,7 @@ def destroy
 end
 
   private
-  def list_params
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 
